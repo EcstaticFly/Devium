9651bef366b6d04473e8fdfa7398aa4ced794775
Suyash Pandey <suyashpandey607@gmail.com>
2025-02-14 17:22:35 +0530

    fixed minor hydration error in theme selector

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -24,6 +24,7 @@ const THEME_ICONS: Record<string, React.ReactNode> = {
 
 function ThemeSelector() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useCodeEditorStore();
   const dropDownRef = useRef<HTMLDivElement>(null);
   const currentTheme = THEMES.find((t) => t.id === theme);
@@ -41,6 +42,16 @@ function ThemeSelector() {
     document.addEventListener("mousedown", handleOutsideClick);
     return () => document.removeEventListener("mousedown", handleOutsideClick);
   }, []);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if(!mounted) return (
+    <div className="w-48 h-12 bg-[#1e1e2e]/80 animate-pulse rounded-lg flex items-center p-2">
+      <div className="w-36 h-6 bg-[#262637]  rounded-md"></div>
+    </div>
+  ); 
 
   return (
     <div className="relative" ref={dropDownRef}>
