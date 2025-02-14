2ca5a7417d0b23c660cbafbb9ba1ff6c04032a31
Suyash Pandey <suyashpandey607@gmail.com>
2025-02-14 21:05:50 +0530

    partially completed editor panel

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -1,9 +1,40 @@
-
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useEffect, useState } from "react";
+import { LANGUAGE_CONFIG } from "../_constants";
+import { Editor } from "@monaco-editor/react";
 
 function EditorPanel() {
+  const [isShareDialogOpen, setIsShareDialogOpen] = useState();
+  const { language, editor, theme, fontSize, setFontSize, setEditor } = useCodeEditorStore();
+
+  useEffect(() => {
+    const savedCode = localStorage.getItem(`editor-code-${language}`);
+    const newCode = savedCode || LANGUAGE_CONFIG[language].defaultCode;
+    if (editor) editor.setValue(newCode);
+  }, [language, editor]);
+
+
+  useEffect(() => {
+    const savedFontSize = localStorage.getItem("editor-font-size");
+    if(savedFontSize) setFontSize(parseInt(savedFontSize));
+  },[setFontSize]);
+
+
+  const handleRefresh = () => {
+
+  }
+
+  const handleEditorChange = () => {
+
+  }
+
+  const handleFontSizeChange = () => {
+
+  }
+
   return (
-    <div>EditorPanel</div>
-  )
+  <div className=""></div>
+);
 }
 
-export default EditorPanel
+export default EditorPanel;
