99ee5e40e35969b22a4c6f74fb86f395721469ec
Suyash Pandey <suyashpandey607@gmail.com>
2025-02-17 18:15:43 +0530

    add navigation header and skeleton components for snippets page

---
+++ src/app/snippets/_components/NavigationHeader.tsx
@@ -0,0 +1,9 @@
+
+
+const NavigationHeader = () => {
+  return (
+    <div>NavigationHeader</div>
+  )
+}
+
+export default NavigationHeader;
--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 
+import { useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useState } from "react";
+import NavigationHeader from "./_components/NavigationHeader";
+import SnippetsPageSkeleton from "./_components/_skeletons/SnippetsPageSkeleton";
 
-export default function page() {
-  return (
-    <div>snippets page</div>
-  )
+export default function SnippetsPage() {
+  const allSnippets = useQuery(api.snippets.getSnippets);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState<string | null>(null);
+  const [view, setView] = useState<"grid" | "list">("grid");
+
+  if (allSnippets === undefined) {
+    //means data is being fetched from convex
+    return (
+      <div className="min-h-screen">
+        <NavigationHeader />
+        <SnippetsPageSkeleton />
+      </div>
+    );
+  }
+  return <div className=""></div>;
 }
