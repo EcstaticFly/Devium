ced2747dd43a0f1d67bf0de895c856e63a41ba87
Suyash Pandey <suyashpandey607@gmail.com>
2025-02-22 04:18:42 +0530

    Add user statistics query to retrieve code execution and starred snippet data

--- convex/codeExecutions.ts
+++ convex/codeExecutions.ts
@@ -48,3 +48,70 @@ export const getUserCodeExecutions = query({
       .paginate(args.paginationOpts);
   },
 });
+
+export const getUserStats = query({
+  args: {
+    userId: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    const executions = await ctx.db
+      .query("codeExecutions")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .collect();
+
+    const starredSnippets = await ctx.db
+      .query("stars")
+      .withIndex("by_user_id")
+      .filter((q) => q.eq(q.field("userId"), args.userId))
+      .collect();
+
+    const snippetsDetails = await Promise.all(
+      starredSnippets.map((starSnippet) => ctx.db.get(starSnippet.snippetId))
+    );
+
+    const starredLanguges = snippetsDetails.filter(Boolean).reduce(
+      (acc, curr) => {
+        if (curr?.language) {
+          acc[curr.language] = (acc[curr.language] || 0) + 1;
+        }
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+    const mostStarredLanguage =
+      Object.entries(starredLanguges).sort(([, a], [, b]) => b - a)[0]?.[0] ??
+      "N/A";
+
+    const last24hrs = executions.filter(
+      (e) => e._creationTime > Date.now() - 24 * 60 * 60 * 1000
+    ).length;
+
+    const languageStats = executions.reduce(
+      (acc, curr) => {
+        acc[curr.language] = (acc[curr.language] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+
+    const languages = Object.keys(languageStats);
+    const favoriteLanguage = languages.length
+      ? languages.reduce((a, b) =>
+          languageStats[a] > languageStats[b] ? a : b
+        )
+      : "N/A";
+
+    return {
+      totalExecutions: executions.length,
+      languagesCount: languages.length,
+      languages: languages,
+      last24hrs,
+      favoriteLanguage,
+      languageStats,
+      mostStarredLanguage,
+    };
+  },
+});
