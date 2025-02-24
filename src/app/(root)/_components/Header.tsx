255148662204e192b63da9066a664f7adc6e1f6d
Suyash Pandey <suyashpandey607@gmail.com>
2025-02-24 15:04:35 +0530

    updated Metadata

--- src/app/(root)/_components/Header.tsx
+++ src/app/(root)/_components/Header.tsx
@@ -26,7 +26,6 @@ async function Header() {
       >
         <div className="hidden lg:flex items-center gap-8">
           <Link href="/" className="flex items-center gap-3 group relative">
-
             <div
               className="absolute -inset-2 bg-gradient-to-r from-blue-500/20 to-purple-500/20 rounded-lg opacity-0 
                 group-hover:opacity-100 transition-all duration-500 blur-xl"
@@ -36,7 +35,7 @@ async function Header() {
               className="relative bg-gradient-to-br from-[#1a1a2e] to-[#0a0a0f] p-2 rounded-xl ring-1
               ring-white/10 group-hover:ring-white/20 transition-all"
             >
-              <Blocks className="size-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
+              <img src="/Devium-logo.svg" alt="devium-logo" className="size-6 text-blue-400 transform -rotate-6 group-hover:rotate-0 transition-transform duration-500" />
             </div>
 
             <div className="flex flex-col">
@@ -44,7 +43,7 @@ async function Header() {
                 Devium
               </span>
               <span className="block text-xs text-blue-400/60 font-medium">
-              Code. Create. Conquer.
+                Code. Create. Conquer.
               </span>
             </div>
           </Link>
